2c95683ab3eb1735db1431ed94e229560e46b807
murcu <murcu.mayer@gmail.com>
2016-09-18 18:08:26 +0200

    camera stuff

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+
+	WebCamTexture mCamera = null;
+
+	// Use this for initialization
+	void Start () {
+		mCamera = new WebCamTexture ();
+
+		transform.GetComponent<Renderer> ().material.mainTexture = mCamera;
+		mCamera.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
